755f8d69dc409a3897ccfae997dcefb253910dbb
U-DESKTOP-2OT72P9\Nirag <niragkadakia@gmail.com>
2020-08-27 22:26:03 -0400

    Measly

--- scripts/run_const_corr.py
+++ scripts/run_const_corr.py
@@ -23,13 +23,14 @@ from local_vars import data_dir
 DATA_DIR = data_dir()
 init_seed = int(sys.argv[1])
 
+epsilons = dict()
 epsilons['gaussian_diag'] = np.arange(0.1, 1.7, 0.05)
 epsilons['quartic_separable'] = np.arange(0.1, 0.3, 0.005)
 
 for pP_type in ['gaussian_diag', 'quartic_separable']:
 	for epsilon in epsilons[pP_type]:
 		
-		a = CHMC(num_walkers=10, nD=50, epsilon=epsilon, 
+		a = CHMC(num_walkers=100, nD=50, epsilon=epsilon, 
 				 num_steps_per_samp=50, num_iterations=1000, 
 				 num_burnin=0)
 		a.set_pX_dist(pX_type='gaussian_const_corr')
@@ -39,8 +40,6 @@ for pP_type in ['gaussian_diag', 'quartic_separable']:
 		a.p_init *= 1.5 
 		
 		a.run()
-		
-		## TODO , calculate errors
 		
 		dir = '%s/errs/%s/%s/%.3f' % (DATA_DIR, a.pX_type, 
 									  a.pP_type, a.epsilon)
